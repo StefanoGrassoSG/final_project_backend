4f1f892a7bc1632c016a2c6edf6c4f2974f2b77c
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-10-13 11:49:33 +0200

    Aggiornamento index style

--- resources/views/admin/apartment/index.blade.php
+++ resources/views/admin/apartment/index.blade.php
@@ -12,39 +12,39 @@
                 <img src="{{ Vite::asset('resources/img/logo_app_plus.png') }}" alt="" class="logo-add-app">
             </div>
             <div class="col-12 mb-4">
-                <a href="{{ route('admin.apartment.create') }}" class="btn btn-outline-primary w-100">
+                <a href="{{ route('admin.apartment.create') }}" class="btn btn-create-form w-100">
                     + Aggiungi
                 </a>
             </div>
             @foreach ($apartments as $singleApt)
-            <div class="card-apartment d-flex my-3 border">
+            <div class="card-apartment d-flex my-3 border rounded-4">
                 <div class="col-3 d-none d-md-block">
                 @if(str_starts_with($singleApt->cover_img,'uploads'))
                 <div class="img-div">
                     <img src="/storage/{{ $singleApt->cover_img }}" alt="{{ $singleApt->name }}">
                 </div>                    
                 @else
-                <div class="img-div">
-                    <img src="{{ $singleApt->cover_img }}" alt="{{ $singleApt->name }}">
+                <div class="img-div ">
+                    <img class="rounded-start-4 " src="{{ $singleApt->cover_img }}" alt="{{ $singleApt->name }}">
                 </div>
                 @endif
                 </div>
-                <div class="col-9 col-md-7 pt-3 ps-3">
+                <div class="col-9 col-md-7 pt-3 ps-3 infos-apt">
                     <h3>
                         Info
                     </h3>
                     <ul>
                         <li>
-                            Nome: {{ $singleApt->name }}
+                            <span class="span-info-apt">Nome: </span>{{ $singleApt->name }}
                         </li>
                         <li>
-                            Indirizzo: {{ $singleApt->address }}
+                            <span class="span-info-apt">Indirizzo: </span>{{ $singleApt->address }}
                         </li>
                         <li>
-                            Metratura: {{ $singleApt->square_meter }}
+                            <span class="span-info-apt">Metratura: </span>{{ $singleApt->square_meter }} mq
                         </li>
                         <li>
-                            Prezzo: {{ $singleApt->price }} &euro;
+                            <span class="span-info-apt">Prezzo: </span>{{ $singleApt->price }} &euro;
                         </li>
                     </ul>
                     
@@ -54,16 +54,16 @@
                         Azioni
                     </h3>
                     <div class="my-2">
-                        <a href="{{ route('admin.apartment.edit', ['apartment' => $singleApt->id]) }}" class="btn btn-primary">Modifica</a>
+                        <a href="{{ route('admin.apartment.edit', ['apartment' => $singleApt->id]) }}" class="btn btn-create-card">Modifica</a>
                     </div>
                     <div class="my-2">
-                        <a href="{{ route('admin.apartment.show', ['apartment' => $singleApt->id]) }}" class="btn btn-primary">Visualizza</a>
+                        <a href="{{ route('admin.apartment.show', ['apartment' => $singleApt->id]) }}" class="btn btn-create-card">Visualizza</a>
                     </div>
                         
                     <form action="{{route('admin.apartment.destroy',['apartment'=>$singleApt->id])}}" method="POST" class="my-1 d-block">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger">
+                        <button type="submit" class="btn btn-create-card btn-create-card-out">
                                 Elimina
                         </button>
                     </form>
