2cac3493bdc1de6740c272fe811ae1e41ad8b953
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-10-13 11:38:28 +0200

    ADD sponsorship

---
+++ resources/views/admin/apartment/sponsorship.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('page-title', 'Dashboard')
+
+@section('main-content')
+    <div class="row">
+        <div class="col">
+          
+        </div>
+    </div>
+@endsection
