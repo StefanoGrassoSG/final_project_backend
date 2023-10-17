f8e9613b3259459f9fd5362cd863cfbd983a4cfb
aalexebr <alex.ebr@icloud.com>
2023-10-17 23:16:31 +0200

    COMPLETE SCSS FILE DIVISION

--- resources/views/admin/apartment/edit.blade.php
+++ resources/views/admin/apartment/edit.blade.php
@@ -65,7 +65,7 @@
 
         {{-- tomtom input --}}
         <div class="d-flex flex-wrap align-items-center">
-			<div id="tom" class="mb-4 col-12 col-md-3 me-5">
+			<div id="tom" class="mb-4 col-12 col-md-3 me-5 w-100">
 			  <label for="tom" class="form-label">Inserisci Indirizzo <span class="text-danger">*</span></label>
 			  {{-- input di tom tom --}}
 			</div>
