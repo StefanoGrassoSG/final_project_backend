317a25726dc08b49eb343ce894ffd12ddb882fc8
aalexebr <alex.ebr@icloud.com>
2023-10-25 19:26:14 +0200

    hotfix statistics

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -49,6 +49,9 @@ class AdminController extends Controller
           ->count();  
 
           $countsByMonth = DB::table('views')
+          ->join('apartments','views.apartment_id','=','apartments.id')
+          ->join('users','apartments.user_id','=','users.id')
+          ->where('user_id', $userId)
           ->selectRaw('DATE_FORMAT(date, "%m") as month, count(*) as count')
           ->groupBy('month')
           ->get();
