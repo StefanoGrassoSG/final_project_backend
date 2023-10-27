d8e23a40e2eba4e38b75b4eb0239133316edf43a
aalexebr <alex.ebr@icloud.com>
2023-10-27 11:43:35 +0200

    config

--- config/apt_addresses.php
+++ config/apt_addresses.php
@@ -7,184 +7,224 @@ return [
         dovete fare una chiamata axios su postman del vostro indirizzo al tomtom con la vostra APIKEY
         indirizzo e APIKEY sono params della chiamata url di tomtom
         e nella risposta json avrete i campi latitudine e longitudine da inserire
+        https://api.tomtom.com/search/2/geocode/INDIRIZZO.json?&key=KEiNGuhsySt5PgvkmCw7C9Sb5vGdacR6
+        nella risposta json trovate 'position' e avete lat e lon
+        in modo alternativo potete andare su laravel inserire indirizzo e andare nel db a ricavarvi lat e lon che usa la stessa funzione della chiamata a tomtom
     */
     [
-        'address' => 'via Vittorio Veneto 14 , Rome',
-        'lat' => '',
-        'lon' => ''
+        'address' => 'via vittorio veneto ,roma',
+        'lat' => '41.90709',
+        'lon' => '12.48991',
+        'cover_img'=> 'resources/img/apt_img/filename'
     ],
     // 7 indirizzi STEFANO
     [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
     [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
+    ],
+   [
+        'address' => '',
+        'lat' => '',
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
+    ],
+   [
+        'address' => '',
+        'lat' => '',
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
     [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
-    ],
-    [
-        'address' => ''
-    ],
-    [
-        'address' => '',
-        'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
     // 7 indirizzi Ale M
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
     // 7 indirizzi PIERPAOLO
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
     //  7 indirizzi EROS
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
     // 7 indirizzi ale ebr
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
-    [
+   [
         'address' => '',
         'lat' => '',
-        'lon' => ''
+        'lon' => '',
+        'cover_img'=> 'resources/img/apt_img/'
     ],
 ];
 
@@ -192,13 +232,13 @@ return [
 /*
     
     
-        $addresses =  config('apt_addresses');
+        $adresses =  config('apt_addresses');
 
         Schema::withoutForeignKeyConstraints(function(){
             Apartment::truncate();
         });
 
-        foreach($singleAddress as $addresses) { 
+        foreach($singleAddress as $adresses) { 
             
             Apartment::create([
                 'room' => fake()->randomDigit(),
@@ -213,7 +253,7 @@ return [
                 'price' => fake()->randomFloat(2, 50, 9999),
                 'square_meter' => fake()->randomNumber(3, false),
                 'description' => fake()->paragraphs(3, true),
-                'cover_img' => fake()->imageUrl(),
+                'cover_img' => $singleAddress['cover_img'],
                 'user_id' => rand(1, 2)
             ]);
         }
